2ce042ef9a75bdab525a67318b5a163c231cb966
Alessandro Febretti <febret@gmail.com>
2015-02-09 20:54:32 -0600

    updated threejs

--- omegaThree.js
+++ omegaThree.js
@@ -1,34 +1,34 @@
 // Namespace
 var omegaThree	= omegaThree || {};
 
-////////////////////////////////////////////////////////////////////////////////
-var onWindowResize = function() {
-    console.log("resize " + window.innerWidth + " " + window.innerHeight)
-    omegaThree.renderer.setSize( window.innerWidth, window.innerHeight );
-}
+w = 0;
+h = 0;
 
 ////////////////////////////////////////////////////////////////////////////////
-omegaThree.init = function() {
+omegaThree.init = function(canvas) {
+    gcanvas = document.getElementById(canvas);
     omegaThree.camera = new THREE.Camera();
     // Disable the camera updateMatrixWorld method, we take care of computing
     // camera transforms in the omegalib runtime.
     omegaThree.camera.updateMatrixWorld = function() {}
-
-    omegaThree.renderer = new THREE.WebGLRenderer();
-    omegaThree.renderer.setSize( window.innerWidth, window.innerHeight);
-
-    container = document.getElementById( 'output' );
-    container.appendChild( omegaThree.renderer.domElement );
-    
-	window.addEventListener('resize', onWindowResize, false);
+    omegaThree.renderer = new THREE.WebGLRenderer({canvas: gcanvas});
+    //omegaThree.renderer.setPixelRatio(0.5)
+    //omegaThree.renderer.setSize( 1920, 1080);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-omegaThree.frame = function() {
+omegaThree.frame = function(omega) {
     omegaThree.camera.projectionMatrix.fromArray(omega.projection);
     omegaThree.camera.matrixWorld.fromArray(omega.modelview);
     
     if(omegaThree.scene) {
+        if(window.innerWidth != w || window.innerHeight != h) {
+            console.log("resize " + window.innerWidth + " " + window.innerHeight)
+            w = window.innerWidth;
+            h = window.innerHeight;
+            omegaThree.renderer.setSize( w,h );
+        }
+
         omegaThree.renderer.render(omegaThree.scene, omegaThree.camera);
     }
 }
